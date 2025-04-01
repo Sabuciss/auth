54b4357c0565be277eeb04e39804cc822cd09aee
unknown <ipb23.s.brikmane@vtdt.edu.lv>
2025-04-01 18:48:25 +0300

    css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
@@ -19,11 +20,11 @@
             @endforeach
         </ul>
     @endif
-  <label for="first_name">Vārds</label><input name="first_name" required>
-  <label for="last_name">Uzvārds</label><input name="last_name" required>
-  <label for="email">e-mail</label><input name="email" type="email" required>
-  <label for="password" >Parole</label><input name="password" type="password" required>
-  <label for="password" >Paroles pārbaude</label><input name="password_confirmation" type="password" required>
+  <label for="first_name">Vārds</label><input name="first_name" required><br>
+  <label for="last_name">Uzvārds</label><input name="last_name" required><br>
+  <label for="email">e-mail</label><input name="email" type="email" required><br>
+  <label for="password" >Parole</label><input name="password" type="password" required><br>
+  <label for="password" >Paroles pārbaude</label><input name="password_confirmation" type="password" required><br>
 
  <button >Saglabāt</button>
 </form>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body> 
    
     @auth
-    <p>Sveiks, {{ Auth::user()->first_name}}</p>
+    <p>Sveiki, {{ Auth::user()->first_name}}</p>
 
         <form action="/logout" method="POST">
            @csrf
