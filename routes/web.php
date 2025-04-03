64a51ed2c31e6fd93181fe18cf5ef39b709c3579
unknown <ipb23.s.brikmane@vtdt.edu.lv>
2025-04-03 09:00:59 +0300

    login

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,17 @@ use App\Http\Controllers\SessionController;
 
 Route::get('/', function () {return view('welcome');});
 
+Route::get('/register', [RegisterController::class, "create"])->middleware("guest");
+
 Route::get('/register', [RegisterController::class, 'create']);
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::post('/logout', [SessionController::class, 'destroy']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, "create"])->middleware("guest");
 Route::post('/login', [SessionController::class, 'store']);
+
+Route::get('/bmw', function () {return view('auth.bmw');})->middleware("guest");
+
 
 
